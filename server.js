497ebc7c23ce0c3a235ac232e0008b099d653dbc
Anugrah0131 <anugrah4840@gamil.com>
2026-05-23 13:20:55 +0530

    now

--- server.js
+++ server.js
@@ -28,16 +28,7 @@ const io = new Server(server, {
   }
 });
 
-<<<<<<< HEAD
-const PORT = process.env.PORT || 7070;
-=======
-app.use(cors({
-  origin: "https://pixora.anfassck.online",
-  credentials: true
-}));
-
-const PORT = 9000;
->>>>>>> a8103e6 (local changes before pull)
+const PORT = process.env.PORT || 9000;
 
 // Socket.io Logic
 const userSockets = new Map(); // userId -> socketId
